36f12d71b4e1e07a0319ccbeebf37d26c9e09609
Caprice-Thompson <caprice.thompson@itv.com>
2024-02-10 18:47:19 +0000

    WIP: Page rating

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
+import StarRating from "../../components/Rating";
 
 function Home() {
-
-    return( 
-        <div className="container">
-    <h1>Welcome to the Project App</h1>
-    <h2> Home of Projects</h2>
-   
+  return (
+    <div className="container">
+      <h1>Welcome to the Project App</h1>
+      <h2> Home of Projects</h2>
+      <StarRating numOfStars={6} />
     </div>
-)
+  );
 }
 
 export default Home;
