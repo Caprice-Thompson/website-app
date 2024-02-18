4a96ad49fd223758c3550446be858dff6c42f950
Caprice-Thompson <caprice.thompson@itv.com>
2024-02-18 15:49:50 +0000

    finish rating set up

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,29 +1,37 @@
-import { FiHeart } from "react-icons/fi";
+import { FaStar } from "react-icons/fa";
 import { useState } from "react";
+import "./styles.css";
 
 export default function StarRating({ numOfStars = 5 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
-  function handleClick(getCurrentIndex) {
-    setRating(getCurrentIndex);
+  function handleClick(index) {
+    if (rating === index) {
+      setRating(0);
+    } else {
+      setRating(index);
+    }
   }
-  function handleMouseEnter(getCurrentIndex) {
-    setHover(getCurrentIndex);
+
+  function handleMouseEnter(index) {
+    setHover(index);
   }
+
   function handleMouseLeave() {
-    setHover(rating);
+    setHover(0);
   }
+
   return (
     <div className="star-rating">
       {[...Array(numOfStars)].map((_, index) => {
-        index += 1;
+        const starValue = index + 1;
         return (
-          <FiHeart
+          <FaStar
             key={index}
-            className={index <= (hover || rating ? "active" : "inactive")}
-            onClick={() => handleClick(index)}
-            onMouseMove={() => handleMouseEnter(index)}
+            className={starValue <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(starValue)}
+            onMouseEnter={() => handleMouseEnter(starValue)}
             onMouseLeave={() => handleMouseLeave()}
             size={40}
           />
